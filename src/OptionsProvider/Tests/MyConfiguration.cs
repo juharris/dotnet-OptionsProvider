102895fd59f29199972d03271aaa84950053e2c8
Justin D. Harris (Dev Box) <juharris@users.noreply.github.com>
2024-06-23 14:37:40 -0400

    Add test

--- src/OptionsProvider/Tests/MyConfiguration.cs
+++ src/OptionsProvider/Tests/MyConfiguration.cs
@@ -1,5 +1,16 @@
 ﻿namespace OptionsProvider.Tests;
 
+/// <summary>
+/// An example configuration for tests.
+/// </summary>
 internal class MyConfiguration
 {
+	public string[]? Array { get; init; }
+	public MyObject? Object { get; init; }
+}
+
+internal class MyObject
+{
+	public int One { get; init; }
+	public double Two { get; init; }
 }
--- src/OptionsProvider/Tests/OptionsProviderTests.cs
+++ src/OptionsProvider/Tests/OptionsProviderTests.cs
@@ -5,18 +5,28 @@ namespace OptionsProvider.Tests;
 [TestClass]
 public class OptionsProviderTests
 {
-	[TestMethod]
-	public async Task Test_OptionsLoader_Async()
+	// Won't be `null` when running tests.
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+	public static IOptionsProvider OptionsProvider { get; private set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+	[AssemblyInitialize]
+	public static async Task ConfigureOptionsProvider(TestContext _)
 	{
-		// TODO Make once for all tests.
 		var configuration = new ConfigurationBuilder()
 			.AddJsonFile("appsettings.json")
 			.Build();
 
 		var loader = new OptionsLoader(configuration);
-		var optionsProvider = await loader.LoadAsync("Configurations");
+		OptionsProvider = await loader.LoadAsync("Configurations");
+	}
 
-		var config = optionsProvider.GetOptions<MyConfiguration>("config");
+	[TestMethod]
+	public void Test_GetOptions_without_Features()
+	{
+		var config = OptionsProvider.GetOptions<MyConfiguration>("config");
 		Assert.IsNotNull(config);
+		config.Array.Should().Equal(["item 1"]);
+		config.Object.Should().BeEquivalentTo(new MyObject { One = 1, Two = 2.0 });
 	}
 }
