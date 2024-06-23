c8bc13afbd29beec9ff613c6726329d6f4442a2a
Justin D. Harris (Dev Box) <juharris@users.noreply.github.com>
2024-06-23 19:21:06 -0400

    Split tests

---
+++ src/OptionsProvider/Tests/OptionsLoaderTests.cs
@@ -0,0 +1,33 @@
+using Microsoft.Extensions.Configuration;
+
+namespace OptionsProvider.Tests;
+
+[TestClass]
+public class OptionsLoaderTests
+{
+	// Won't be `null` when running tests.
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+	public static IOptionsProvider OptionsProvider;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+
+	[AssemblyInitialize]
+	public static async Task ConfigureOptionsProvider(TestContext _)
+	{
+		var configuration = new ConfigurationBuilder()
+			.AddJsonFile("appsettings.json")
+			.Build();
+
+		var loader = new OptionsLoader(configuration);
+		OptionsProvider = await loader.LoadAsync("Configurations");
+	}
+
+	[TestMethod]
+	public async Task Test_LoadAsync_with_Existing_Name()
+	{
+		var loader = new OptionsLoader(new ConfigurationBuilder().Build());
+		var action = () => loader.LoadAsync("InvalidConfigurations");
+		await action.Should().ThrowAsync<InvalidOperationException>()
+			.WithMessage($"The name \"other example\" for the configuration file \"{Path.Combine("InvalidConfigurations", "other example.json")}\" is already used.");
+	}
+}
--- src/OptionsProvider/Tests/OptionsProviderTests.cs
+++ src/OptionsProvider/Tests/OptionsProviderTests.cs
@@ -1,15 +1,8 @@
-using Microsoft.Extensions.Configuration;
-
 namespace OptionsProvider.Tests;
 
 [TestClass]
 public class OptionsProviderTests
 {
-	// Won't be `null` when running tests.
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-	private static IOptionsProvider OptionsProvider;
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-
 	private static readonly MyConfiguration DefaultMyConfiguration = new()
 	{
 		Array = ["item 1"],
@@ -27,29 +20,18 @@ public class OptionsProviderTests
 		Array = ["sub_example item 1", "sub_example item 2"],
 		Object = new MyObject { One = 11, Two = 22, Three = 3, },
 	};
-
-	[AssemblyInitialize]
-	public static async Task ConfigureOptionsProvider(TestContext _)
-	{
-		var configuration = new ConfigurationBuilder()
-			.AddJsonFile("appsettings.json")
-			.Build();
-
-		var loader = new OptionsLoader(configuration);
-		OptionsProvider = await loader.LoadAsync("Configurations");
-	}
-
+	
 	[TestMethod]
 	public void Test_GetOptions_No_Config()
 	{
-		var config = OptionsProvider.GetOptions<MyConfiguration>("does not exist");
+		var config =  OptionsLoaderTests.OptionsProvider.GetOptions<MyConfiguration>("does not exist");
 		Assert.IsNull(config);
 	}
 
 	[TestMethod]
 	public void Test_GetOptions_without_Features()
 	{
-		var config = OptionsProvider.GetOptions<MyConfiguration>("config");
+		var config = OptionsLoaderTests.OptionsProvider.GetOptions<MyConfiguration>("config");
 		Assert.IsNotNull(config);
 		config.Should().BeEquivalentTo(DefaultMyConfiguration);
 	}
@@ -57,11 +39,11 @@ public class OptionsProviderTests
 	[TestMethod]
 	public void Test_GetOptions_with_Features()
 	{
-		var config = OptionsProvider.GetOptions<MyConfiguration>("config", ["example"]);
+		var config = OptionsLoaderTests.OptionsProvider.GetOptions<MyConfiguration>("config", ["example"]);
 		Assert.IsNotNull(config);
 		config.Should().BeEquivalentTo(ExampleMyConfiguration);
 
-		config = OptionsProvider.GetOptions<MyConfiguration>("config", ["sub_example"]);
+		config = OptionsLoaderTests.OptionsProvider.GetOptions<MyConfiguration>("config", ["sub_example"]);
 		Assert.IsNotNull(config);
 		config.Should().BeEquivalentTo(SubExampleMyConfiguration);
 	}
@@ -69,53 +51,43 @@ public class OptionsProviderTests
 	[TestMethod]
 	public void Test_GetOptions_For_Deep_Key()
 	{
-		var array = OptionsProvider.GetOptions<string[]>("config:array", ["example"]);
+		var array = OptionsLoaderTests.OptionsProvider.GetOptions<string[]>("config:array", ["example"]);
 		Assert.IsNotNull(array);
 		array.Should().Equal(ExampleMyConfiguration.Array);
 
-		var one = OptionsProvider.GetOptions<int>("config:object:one", ["sub_example"]);
+		var one = OptionsLoaderTests.OptionsProvider.GetOptions<int>("config:object:one", ["sub_example"]);
 		one.Should().Be(SubExampleMyConfiguration.Object!.One);
 	}
-
 
 	[TestMethod]
 	public void Test_GetOptions_with_Unknown_Feature()
 	{
-		var action = () => OptionsProvider.GetOptions<MyConfiguration>("config", ["unknown"]);
+		var action = () => OptionsLoaderTests.OptionsProvider.GetOptions<MyConfiguration>("config", ["unknown"]);
 		action.Should().Throw<InvalidOperationException>()
 			.WithMessage("The given feature name \"unknown\" is not a known feature.");
 	}
 
 	[TestMethod]
-	public async Task Test_LoadAsync_with_Existing_Name()
-	{
-		var loader = new OptionsLoader(new ConfigurationBuilder().Build());
-		var action = () => loader.LoadAsync("InvalidConfigurations");
-		await action.Should().ThrowAsync<InvalidOperationException>()
-			.WithMessage($"The name \"other example\" for the configuration file \"{Path.Combine("InvalidConfigurations", "other example.json")}\" is already used.");
-	}
-
-	[TestMethod]
 	public void Test_GetOptions_Same_Instance_without_Features()
 	{
-		var config1 = OptionsProvider.GetOptions<MyConfiguration>("config");
-		var config2 = OptionsProvider.GetOptions<MyConfiguration>("config");
+		var config1 = OptionsLoaderTests.OptionsProvider.GetOptions<MyConfiguration>("config");
+		var config2 = OptionsLoaderTests.OptionsProvider.GetOptions<MyConfiguration>("config");
 		Assert.AreSame(config1, config2);
 	}
 
 	[TestMethod]
 	public void Test_GetOptions_Same_Instance()
 	{
-		var config1 = OptionsProvider.GetOptions<MyConfiguration>("config", ["example"]);
-		var config2 = OptionsProvider.GetOptions<MyConfiguration>("config", ["eXamPlE"]);
+		var config1 = OptionsLoaderTests.OptionsProvider.GetOptions<MyConfiguration>("config", ["example"]);
+		var config2 = OptionsLoaderTests.OptionsProvider.GetOptions<MyConfiguration>("config", ["eXamPlE"]);
 		Assert.AreSame(config1, config2);
 	}
 
 	[TestMethod]
 	public void Test_GetOptions_Same_Instance_With_AlternativeName()
 	{
-		var config1 = OptionsProvider.GetOptions<MyConfiguration>("config", ["subdir/example"]);
-		var config2 = OptionsProvider.GetOptions<MyConfiguration>("config", ["sub_eXaMpLe"]);
+		var config1 = OptionsLoaderTests.OptionsProvider.GetOptions<MyConfiguration>("config", ["subdir/example"]);
+		var config2 = OptionsLoaderTests.OptionsProvider.GetOptions<MyConfiguration>("config", ["sub_eXaMpLe"]);
 		Assert.AreSame(config1, config2);
 	}
 }
