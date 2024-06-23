c4305d79a64f9eb1595a9e1cc1ac372b7852ab53
Justin D. Harris (Dev Box) <juharris@users.noreply.github.com>
2024-06-23 15:00:53 -0400

    Add tests

--- src/OptionsProvider/Tests/OptionsProviderTests.cs
+++ src/OptionsProvider/Tests/OptionsProviderTests.cs
@@ -22,11 +22,50 @@ public class OptionsProviderTests
 	}
 
 	[TestMethod]
+	public void Test_GetOptions_No_Config()
+	{
+		var config = OptionsProvider.GetOptions<MyConfiguration>("does not exist");
+		Assert.IsNull(config);
+	}
+
+	[TestMethod]
 	public void Test_GetOptions_without_Features()
 	{
 		var config = OptionsProvider.GetOptions<MyConfiguration>("config");
 		Assert.IsNotNull(config);
 		config.Array.Should().Equal(["item 1"]);
 		config.Object.Should().BeEquivalentTo(new MyObject { One = 1, Two = 2.0 });
+	}
+
+	// TODO Test some values.
+	[DataRow]
+	[DataRow("example")]
+	[DataRow("subdir/example")]
+	[DataRow("sub_example")]
+	[TestMethod]
+	public void Test_GetOptions_with_Features(params string[] features)
+	{
+		var config = OptionsProvider.GetOptions<MyConfiguration>("config", features);
+		Assert.IsNotNull(config);
+		config.Array.Should().Equal(["item 1"]);
+		config.Object.Should().BeEquivalentTo(new MyObject { One = 1, Two = 2.0 });
+	}
+
+	[TestMethod]
+	[Ignore("Caching logic is not implemented yet.")]
+	public void Test_GetOptions_Same_Instance()
+	{
+		var config1 = OptionsProvider.GetOptions<MyConfiguration>("config", ["example"]);
+		var config2 = OptionsProvider.GetOptions<MyConfiguration>("config", ["example"]);
+		Assert.AreSame(config1, config2);
+	}
+
+	[TestMethod]
+	[Ignore("Caching logic is not implemented yet.")]
+	public void Test_GetOptions_Same_Instance_With_AlternativeName()
+	{
+		var config1 = OptionsProvider.GetOptions<MyConfiguration>("config", ["subdir/example"]);
+		var config2 = OptionsProvider.GetOptions<MyConfiguration>("config", ["sub_example"]);
+		Assert.AreSame(config1, config2);
 	}
 }
