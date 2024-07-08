61d27aae56de9c85e9fb38d4f1f7afe2e5f63f7f
Justin D. Harris <juharris@users.noreply.github.com>
2024-07-08 14:49:49 -0400

    Support building MemoryCacheEntryOptions

---
+++ src/OptionsProvider/OptionsProvider/CacheOptionsBuilderInput.cs
@@ -0,0 +1,17 @@
+﻿namespace OptionsProvider;
+
+/// <summary>
+/// Input to a function to dynamically create <see cref="Microsoft.Extensions.Caching.Memory.MemoryCacheEntryOptions"/> for <see cref="ServiceCollectionExtensions.ConfigureOptions"/>.
+/// </summary>
+public sealed class CacheOptionsBuilderInput
+{
+	/// <summary>
+	/// The path to the options in the configurations.
+	/// </summary>
+	public string? OptionsKey { get; init; }
+
+	/// <summary>
+	/// Indicates the features that are enabled for the current scope.
+	/// </summary>
+	public required IFeaturesContext FeaturesContext { get; init; }
+}
