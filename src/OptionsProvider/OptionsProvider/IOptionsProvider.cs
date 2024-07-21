a75c331b35b7d5c9c51c987369e9c533a1ea84d4
Justin D. Harris <juharris@users.noreply.github.com>
2024-07-20 23:40:33 -0400

    Improve docs

--- src/OptionsProvider/OptionsProvider/IOptionsProvider.cs
+++ src/OptionsProvider/OptionsProvider/IOptionsProvider.cs
@@ -7,13 +7,20 @@ namespace OptionsProvider;
 /// </summary>
 public interface IOptionsProvider
 {
-	/// <returns>The mapping from alias to feature name.</returns>
+	/// <returns>
+	/// The mapping from alias to feature name.
+	/// Keys include the feature names that may be derived from file names.
+	/// </returns>
 	IDictionary<string, string> GetAliasMapping();
 
-	/// <returns>The known feature names, but not aliases.</returns>
+	/// <returns>
+	/// The known feature names, but not aliases.
+	/// </returns>
 	ICollection<string> GetFeatureNames();
 
-	/// <returns>The metadata for each feature.</returns>
+	/// <returns>
+	/// The metadata for each feature.
+	/// </returns>
 	IDictionary<string, OptionsMetadata> GetMetadataMapping();
 
 	/// <summary>
