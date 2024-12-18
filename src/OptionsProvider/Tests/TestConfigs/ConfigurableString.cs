cd9f5d41b72290cc63a45e5b9af2a5584c28fbcc
Justin D. Harris <juharris@users.noreply.github.com>
2024-12-18 15:00:51 -0500

    Stub

--- src/OptionsProvider/Tests/TestConfigs/ConfigurableString.cs
+++ src/OptionsProvider/Tests/TestConfigs/ConfigurableString.cs
@@ -1,4 +1,14 @@
 ﻿namespace OptionsProvider.Tests.TestConfigs;
 internal sealed class ConfigurableString
 {
+	public ConfigurableString(
+		string template,
+		IReadOnlyDictionary<string, string> values)
+	{
+		// TODO Use a library to replace the values.
+		this.Value = template + values.ToString();
+	}
+
+	// TODO Use Lazy?
+	public string? Value { get; set; }
 }
